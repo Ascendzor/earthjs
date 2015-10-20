0cd4393c02217f2ad81346c2dc4408f8870250ae
Ascendzor <wow.tmare@gmail.com>
2015-10-21 11:27:32 +1300

    no moar node modules yo

--- bundle.js
+++ bundle.js
@@ -36753,27 +36753,12 @@ module.exports = {
     earthHandle = earth(containerId);
   },
   addLayer: function(layer) {
-    console.log('only adds graticule for now, needs to be made extendable');
     earthHandle.addLayer(layer);
   },
   removeLayer: function(layer) {
-    console.log('only removes graticule for now, needs to be made extendable');
     earthHandle.removeLayer(layer);
   }
 }
-
-// var graticuleLayer = require('interactive-earth-graticule');
-// var earthHandle = earth('earthContainer2')
-// setTimeout(function() {
-//   earthHandle
-//     //.setProjection('equirectangular')
-//     .addLayer(graticuleLayer);
-// }, 2000);
-//
-// setTimeout(function() {
-//   earthHandle
-//     .removeLayer(graticuleLayer);
-// }, 4000);
 
 },{"./earth.js":4}],14:[function(require,module,exports){
 /**
