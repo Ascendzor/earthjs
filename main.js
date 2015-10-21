6467e5436ca36f387e4589614ea65a1d6bd75461
Ascendzor <wow.tmare@gmail.com>
2015-10-21 14:44:12 +1300

    included points layer

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var interactiveEarth = require('interactive-earth');
 var graticuleLayer = require('interactive-earth-graticule');
+var pointsLayer = require('interactive-earth-points');
+
+var lightningOptions = {
+  get: '',
+  color: 'red',
+  pointDuration: 10
+}
+var lightningPointsLayer = pointsLayer(lightningOptions)
 interactiveEarth.drawEarth('earthContainer');
 
 setTimeout(function() {
@@ -9,3 +17,7 @@ setTimeout(function() {
 setTimeout(function() {
   interactiveEarth.removeLayer(graticuleLayer);
 }, 4000);
+
+setTimeout(function() {
+  interactiveEarth.addLayer(lightningPointsLayer);
+}, 1000);
